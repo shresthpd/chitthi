3e28785554d3da3c44c623ab5e493ccce073cf94
Shresth Prasad <prasadshresth04@gmail.com>
2025-12-28 14:17:27 +0530

    handle real chat messages instead of dummy data

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect, useRef } from "react";
-import assets, { messagesDummyData } from "../assets/assets";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import assets from "../assets/assets";
 import { formatTime } from "../lib/utils.js";
+import { ChatContext } from "../../context/ChatContext.jsx";
+import { AuthContext } from "../../context/AuthContext.jsx";
+import toast from "react-hot-toast";
 
-const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+const ChatContainer = () => {
   const scrollEnd = useRef(null);
   const chatAreaRef = useRef(null);
-
+  const { messages, selectedUser, setSelectedUser, getMessages, sendMessage } =
+    useContext(ChatContext);
+  const { authUser, onlineUsers } = useContext(AuthContext);
+  const [messageInput, setMessageInput] = useState("");
   const scrollToBottom = (smooth = true) => {
     if (!scrollEnd.current) return;
     requestAnimationFrame(() => {
@@ -15,15 +21,52 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
     });
   };
 
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+
+    if (!selectedUser) {
+      toast.error("Please select a user to chat with.");
+      return;
+    }
+
+    const content = messageInput.trim();
+    if (content === "") return;
+
+    await sendMessage({ text: content });
+    setMessageInput("");
+    scrollToBottom();
+  };
+
+  const handleSendingImage = async (e) => {
+    const file = e.target.files[0];
+    if (!file || !file.type.startsWith("image/")) {
+      toast.error("Invalid file type!");
+      return;
+    }
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      await sendMessage({ image: reader.result });
+      e.target.value = "";
+      scrollToBottom();
+    };
+    reader.readAsDataURL(file);
+  };
+
   useEffect(() => {
     scrollToBottom(false);
-  }, [selectedUser, messagesDummyData.length]);
+  }, [selectedUser, messages]);
 
   useEffect(() => {
     if (chatAreaRef.current) {
       scrollToBottom(false);
     }
   }, []);
+
+  useEffect(() => {
+    if (selectedUser) {
+      getMessages(selectedUser._id);
+    }
+  }, [selectedUser]);
 
   if (!selectedUser) {
     return (
@@ -44,11 +87,17 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
           onClick={() => setSelectedUser(null)}
         />
 
-        <img src={assets.profile_martin} alt="" className="rounded-full w-9" />
+        <img
+          src={selectedUser.profilePic || assets.avatar_icon}
+          alt=""
+          className="rounded-full w-9"
+        />
 
         <p className="text-lg text-white flex-1 ml-4 font-serif flex items-center gap-2">
-          Martin Johnson
-          <span className="w-2 h-2 rounded-full bg-green-400" />
+          {selectedUser.fullName}
+          {onlineUsers.includes(selectedUser._id) && (
+            <span className="w-2 h-2 rounded-full bg-green-400" />
+          )}
         </p>
 
         <img src={assets.help_icon} alt="" className="rounded-full w-5" />
@@ -59,13 +108,11 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         ref={chatAreaRef}
         className="flex-1 overflow-y-auto p-3 flex flex-col gap-4"
       >
-        {messagesDummyData.map((msg, index) => (
+        {messages.map((msg, index) => (
           <div
             key={index}
             className={`flex justify-end gap-2 ${
-              msg.senderId !== "680f50e4f10f3cd28382ecf9"
-                ? "flex-row-reverse"
-                : ""
+              msg.senderId !== authUser._id ? "flex-row-reverse" : ""
             }`}
           >
             {msg.image ? (
@@ -78,7 +125,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             ) : (
               <p
                 className={`max-w-[250px] rounded-lg p-3 font-light text-white ${
-                  msg.senderId !== "680f50e4f10f3cd28382ecf9"
+                  msg.senderId !== authUser._id
                     ? "bg-slate-600 rounded-bl-none"
                     : "bg-purple-600 rounded-br-none"
                 }`}
@@ -90,9 +137,9 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             <div className="flex flex-col items-center justify-end">
               <img
                 src={
-                  msg.senderId === "680f50e4f10f3cd28382ecf9"
-                    ? assets.avatar_icon
-                    : assets.profile_martin
+                  msg.senderId === authUser._id
+                    ? authUser.profilePic || assets.avatar_icon
+                    : selectedUser.profilePic || assets.avatar_icon
                 }
                 className="w-8 h-8 rounded-full"
                 alt=""
@@ -111,6 +158,9 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
       <div className="flex-none w-full flex items-center px-3 py-2">
         <div className="w-full px-5 py-3 rounded-4xl bg-slate-700 text-white placeholder:text-slate-400 flex items-center gap-3 mr-2">
           <input
+            onChange={(e) => setMessageInput(e.target.value)}
+            value={messageInput}
+            onKeyDown={(e) => (e.key === "Enter" ? handleSendMessage(e) : null)}
             type="text"
             placeholder="Type a message"
             className="w-full bg-transparent outline-none"
@@ -121,6 +171,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             id="image"
             hidden
             accept="image/png,image/jpeg,image/jpg"
+            onChange={handleSendingImage}
           />
           <label htmlFor="image">
             <img
@@ -135,8 +186,8 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
           src={assets.send_button}
           alt="Send"
           className="w-10 h-10 cursor-pointer"
-          onClick={() => {
-            scrollToBottom();
+          onClick={(e) => {
+            handleSendMessage(e);
           }}
         />
       </div>
